5e2bf963306ddca201a0c746cbf62d8e48182138
metinbaris <hizmetparki@gmail.com>
2024-03-12 11:41:38 +0300

    Update blade views

---
+++ resources/views/base.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AI Workout Suggester</title>
+    <link href="https://cdn.jsdelivr.net/npm/@ionic/core@latest/css/ionic.bundle.css" rel="stylesheet">
+    <link href="/src/css/app.css" rel="stylesheet">
+</head>
+<body>
+    
+    <ion-app>
+        @yield('content')
+    </ion-app>
+
+    <script src="https://cdn.jsdelivr.net/npm/@ionic/core@latest/dist/ionic/ionic.js"></script>
+</body>
+</html>
