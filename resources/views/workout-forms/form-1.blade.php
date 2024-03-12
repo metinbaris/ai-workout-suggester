9a4564e1e90039e1e32b4f378faf344e08ff5652
metinbaris <hizmetparki@gmail.com>
2024-03-12 14:58:51 +0300

    Update form input ids and names

--- resources/views/workout-forms/form-1.blade.php
+++ resources/views/workout-forms/form-1.blade.php
@@ -1,14 +1,14 @@
 <ion-form id="form-one" class="workout-form">
     <!-- Age Input -->
     <ion-item>
-        <ion-label position="floating" required>Age</ion-label>
-        <ion-input type="number" placeholder="Enter your age" required></ion-input>
+        <ion-label position="floating">Age</ion-label>
+        <ion-input id="age" name="age" type="number" placeholder="Enter your age" required></ion-input>
     </ion-item>
 
     <!-- Gender Input -->
     <ion-item>
         <ion-label>Gender</ion-label>
-        <ion-select interface="popover" placeholder="Select One" required>
+        <ion-select id="gender" name="gender" interface="popover" placeholder="Select One" required>
             <ion-select-option value="female">Female</ion-select-option>
             <ion-select-option value="male">Male</ion-select-option>
             <ion-select-option value="non-binary">Non-Binary</ion-select-option>
--- resources/views/workout-forms/form-2.blade.php
+++ resources/views/workout-forms/form-2.blade.php
@@ -2,13 +2,13 @@
     <!-- Height Input -->
     <ion-item>
         <ion-label position="floating" required>Height (cm)</ion-label>
-        <ion-input type="number" placeholder="Enter your height"></ion-input>
+        <ion-input id="height" name="height" type="number" placeholder="Enter your height"></ion-input>
     </ion-item>
 
     <!-- Weight Input -->
     <ion-item>
         <ion-label position="floating" required>Weight (kg)</ion-label>
-        <ion-input type="number" placeholder="Enter your weight"></ion-input>
+        <ion-input id="weight" name="weight" type="number" placeholder="Enter your weight"></ion-input>
     </ion-item>
 
     <!-- Weekly Exercise Amount -->
@@ -20,22 +20,22 @@
 
             <ion-item>
                 <ion-label>None</ion-label>
-                <ion-radio slot="start" value="none"></ion-radio>
+                <ion-radio id="workout-amount-0" name="workout-amount" slot="start" value="none"></ion-radio>
             </ion-item>
 
             <ion-item>
                 <ion-label>1-2 days</ion-label>
-                <ion-radio slot="start" value="1-2"></ion-radio>
+                <ion-radio id="workout-amount-1" name="workout-amount" slot="start" value="1-2"></ion-radio>
             </ion-item>
 
             <ion-item>
                 <ion-label>3-4 days</ion-label>
-                <ion-radio slot="start" value="3-4"></ion-radio>
+                <ion-radio id="workout-amount-2" name="workout-amount" slot="start" value="3-4"></ion-radio>
             </ion-item>
 
             <ion-item>
                 <ion-label>5+ days</ion-label>
-                <ion-radio slot="start" value="5+"></ion-radio>
+                <ion-radio id="workout-amount-3" name="workout-amount" slot="start" value="5+"></ion-radio>
             </ion-item>
         </ion-radio-group>
     </ion-list>
