33ec5efcbda36494f70ae01c49fe003779977b76
metinbaris <hizmetparki@gmail.com>
2024-03-12 15:24:56 +0300

    Add frontend validation to form-1

--- resources/js/workout-form.js
+++ resources/js/workout-form.js
@@ -1,13 +1,49 @@
-const nextButton = document.getElementById('form-one-btn')
-const backButton = document.getElementById('form-back-btn')
+class WorkoutForm {
+    constructor() {
+        this.nextButton = document.getElementById('form-one-btn');
+        this.backButton = document.getElementById('form-back-btn');
 
-nextButton.addEventListener('click', toggleForm)
-backButton.addEventListener('click', toggleForm)
+        this.addEventListeners();
+    }
 
-function toggleForm(event) {
-    const formOne = document.getElementById('form-one')
-    const formTwo = document.getElementById('form-two')
+    addEventListeners() {
+        this.nextButton.addEventListener('click', () => this.showSecondForm());
+        this.backButton.addEventListener('click', () => this.toggleForm());
+    }
 
-    formOne?.toggleAttribute('hidden')
-    formTwo?.toggleAttribute('hidden')
+    toggleForm() {
+        const formOne = document.getElementById('form-one');
+        const formTwo = document.getElementById('form-two');
+
+        formOne?.toggleAttribute('hidden');
+        formTwo?.toggleAttribute('hidden');
+    }
+
+    validateFirstForm() {
+        const ageInput = document.getElementById('age');
+        const age = parseInt(ageInput.value);
+
+        if (isNaN(age) || age < 18 || age > 90) {
+            alert('Invalid age. Please enter a number between 18 and 90.');
+            return false;
+        }
+
+        const gender = document.getElementById('gender').value;
+        if (!gender) {
+            alert('Please select your gender.');
+            return false;
+        }
+
+        return true;
+    }
+
+    showSecondForm() {
+        const isFirstFormValid = this.validateFirstForm();
+
+        if (isFirstFormValid) {
+            this.toggleForm();
+        }
+    }
 }
+
+new WorkoutForm();
--- resources/views/workout-forms/form-1.blade.php
+++ resources/views/workout-forms/form-1.blade.php
@@ -1,13 +1,13 @@
 <ion-form id="form-one" class="workout-form">
     <!-- Age Input -->
     <ion-item>
-        <ion-label position="floating">Age</ion-label>
+        <ion-label position="floating">Age*</ion-label>
         <ion-input id="age" name="age" type="number" placeholder="Enter your age" required></ion-input>
     </ion-item>
 
     <!-- Gender Input -->
     <ion-item>
-        <ion-label>Gender</ion-label>
+        <ion-label>Gender*</ion-label>
         <ion-select id="gender" name="gender" interface="popover" placeholder="Select One" required>
             <ion-select-option value="female">Female</ion-select-option>
             <ion-select-option value="male">Male</ion-select-option>
