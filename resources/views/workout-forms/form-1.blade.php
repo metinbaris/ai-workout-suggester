480753d83d4e6a1bb0933e72f609fae5850f3ae9
metinbaris <hizmetparki@gmail.com>
2024-03-13 13:02:40 +0300

    Add workout goal to form 1

--- resources/views/workout-forms/form-1.blade.php
+++ resources/views/workout-forms/form-1.blade.php
@@ -15,5 +15,34 @@
         </ion-select>
     </ion-item>
 
+    <ion-list>
+        <ion-radio-group>
+            <ion-list-header>
+                <ion-label>Fitness goal?</ion-label>
+            </ion-list-header>
+
+            <ion-item>
+                <ion-label>Dont Know Yet</ion-label>
+                <ion-radio id="workout-goal-0" name="goal" slot="start" value="General fitness improvement"></ion-radio>
+            </ion-item>
+
+            <ion-item>
+                <ion-label>Weight Loss</ion-label>
+                <ion-radio id="workout-goal-1" name="goal" slot="start" value="Weight loss"></ion-radio>
+            </ion-item>
+
+            <ion-item>
+                <ion-label>Muscle Gain</ion-label>
+                <ion-radio id="workout-goal-2" name="goal" slot="start" value="Muscle gain"></ion-radio>
+            </ion-item>
+
+            <ion-item>
+                <ion-label>Endurance Improvement</ion-label>
+                <ion-radio id="workout-goal-3" name="goal" slot="start" value="Endurance improvement"></ion-radio>
+            </ion-item>
+
+        </ion-radio-group>
+    </ion-list>
+
     <ion-button id="form-one-btn" type="button" expand="full">Next</ion-button>
 </ion-form>
