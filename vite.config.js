af5ab3860c101cff98616ca415794814d691e544
metinbaris <hizmetparki@gmail.com>
2024-03-18 11:20:17 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,14 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/results.css',
+                'resources/css/workout-form.css',
+                'resources/js/ion-validation.js',
+                'resources/js/workout-form.js',
+                'resources/js/results.js'
+            ],
             refresh: true,
         }),
     ],
