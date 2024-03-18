c03321d1850728fee31b1fcea9dafa05ac8eb162
metinbaris <hizmetparki@gmail.com>
2024-03-18 11:50:55 +0300

    Update gpt model

--- app/Http/Services/SuggestionAIService.php
+++ app/Http/Services/SuggestionAIService.php
@@ -32,7 +32,7 @@ class SuggestionAIService {
 
         $response = Http::withToken(env('OPEN_AI_SECRET'))
             ->post('https://api.openai.com/v1/chat/completions', [
-                "model" => "gpt-4",
+                "model" => "gpt-3.5-turbo",
                 "messages" => [
                     [
                         "role" => "system",
